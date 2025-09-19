bfc4c561a7197bc324701bbb64c840e6dd0af2a1
itdavidivan <149909071+itdavidivan@users.noreply.github.com>
2025-09-19 22:39:09 +0200

    logout-button changed

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -445,8 +445,14 @@ input[type="number"] {
   }
 
   .logout-button {
-    font-size: 0.9rem;
-    margin: 0;
+    position: absolute; /* aby bol nad dashboardom */
+    top: 2px; /* ešte bližšie k vrchu */
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 0.5rem 0.8rem; /* menší padding */
+    font-size: 0.85rem;
+    margin: 0; /* odstráni default margin */
+    z-index: 1000;
   }
 }
 
