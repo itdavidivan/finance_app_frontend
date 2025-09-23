55f9cf0fd1de33e5aace540620ba2b85db43bfab
itdavidivan <149909071+itdavidivan@users.noreply.github.com>
2025-09-23 11:51:11 +0200

    created manifest for icon on desktop

--- app/app.vue
+++ app/app.vue
@@ -3,6 +3,13 @@
     <nuxt-page></nuxt-page>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  link: [{ rel: "manifest", href: "/manifest.json" }],
+  meta: [{ name: "theme-color", content: "#1e3c72" }],
+});
+</script>
+
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap");
 
